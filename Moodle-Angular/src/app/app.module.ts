797b26684f36971b861f040644c79741f0d19b89
AdamDanniel <dadam4696@gmail.com>
2025-05-21 11:15:08 +0200

    component participant post

--- Moodle-Angular/src/app/app.module.ts
+++ Moodle-Angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { ParticipantsComponent } from './participants/participants.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    ParticipantsComponent
   ],
   imports: [
     BrowserModule
