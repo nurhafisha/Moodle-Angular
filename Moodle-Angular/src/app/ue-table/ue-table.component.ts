ec2a7d0be0060af5b31c42b04832e191f6c24e36
Akmal <akmalsabri73@gmail.com>
2025-05-24 20:01:21 +0200

    update pageAdmin style

--- Moodle-Angular/src/app/ue-table/ue-table.component.ts
+++ Moodle-Angular/src/app/ue-table/ue-table.component.ts
@@ -7,9 +7,19 @@ import { Component } from '@angular/core';
 })
 export class UETableComponent {
   ues = [
-    { code: 'UE101', name: 'Mathematics', image: 'math.jpg' },
-    { code: 'UE102', name: 'Physics', image: 'physics.jpg' },
-    { code: 'UE103', name: 'Chemistry', image: 'chemistry.jpg' },
+    { code: 'IT44', name: 'Analyse Numérique', image: 'math.jpg' },
+    { code: 'SI40', name: "Système d'information", image: 'physics.jpg' },
+    { code: 'SY43', name: 'Android Developpement', image: 'chemistry.jpg' },
+    { code: 'DE52', name: 'Génie logiciel', image: 'chemistry.jpg' },
+    {
+      code: 'SY41',
+      name: 'Informatique embarquée et robotique: Microprocesseurs et microcontrolleurs',
+      image: 'chemistry.jpg',
+    },
+    { code: 'WE4B', name: 'Technologies WEB avancées', image: 'chemistry.jpg' },
+    { code: 'VA51', name: "Traitement de l'image", image: 'chemistry.jpg' },
+    { code: 'RS40', name: 'Réseaux et Cybersécurité', image: 'chemistry.jpg' },
+    { code: 'HM40', name: 'Interface Homme Machine', image: 'chemistry.jpg' },
   ];
 
   constructor() {}
