9c7621a886f91b4d60b93c483b251e7db9c974d9
fisha <nurhafisha15@gmail.com>
2025-05-24 21:09:33 +0200

    add - app-routing.module.ts

---
+++ Moodle-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './components/form/form.component'; // Import the FormComponent
+
+// Define routes
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  {path: 'form' , component: FormComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot(routes)  
+  ],
+  exports: [RouterModule]  
+})
+export class AppRoutingModule { }
