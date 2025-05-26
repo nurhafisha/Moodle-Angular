c00593e9da97231d44017c51fa564b5bf3b53745
AdamDanniel <dadam4696@gmail.com>
2025-05-27 00:07:48 +0200

    discussion

--- Moodle-Angular/src/app/app.module.ts
+++ Moodle-Angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContenuUeComponent } from './pages/contenu-ue/contenu-ue.component';
-import { PostComponent } from './components/post/post.component';
+import { PostComponent } from './components/section/post/post.component';
 import { DiscussionsComponent } from './components/discussions/discussions.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { FormComponent } from './components/form/form.component';
@@ -19,6 +19,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageadminComponent } from './pages/page-admin/page-admin.component';
 import { UserTableComponent } from './components/user-table/user-table.component';
 import { UETableComponent } from './components/ue-table/ue-table.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { UETableComponent } from './components/ue-table/ue-table.component';
     PageadminComponent,
     UserTableComponent,
     UETableComponent,
+    SectionComponent,
   ],
 
   imports: [
