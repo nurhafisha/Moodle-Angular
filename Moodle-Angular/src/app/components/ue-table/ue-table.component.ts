bd3a9d1e71787578bfa338dc4255eeb60762e9d8
Akmal <akmalsabri73@gmail.com>
2025-05-29 22:11:41 +0200

    fix navbar to every page

--- Moodle-Angular/src/app/components/ue-table/ue-table.component.ts
+++ Moodle-Angular/src/app/components/ue-table/ue-table.component.ts
@@ -7,19 +7,18 @@ import { Component } from '@angular/core';
 })
 export class UETableComponent {
   ues = [
-    { code: 'IT44', name: 'Analyse Numérique', image: 'math.jpg' },
-    { code: 'SI40', name: "Système d'information", image: 'physics.jpg' },
-    { code: 'SY43', name: 'Android Developpement', image: 'chemistry.jpg' },
-    { code: 'DE52', name: 'Génie logiciel', image: 'chemistry.jpg' },
+    { code: 'IT44', name: 'Analyse Numérique' },
+    { code: 'SI40', name: "Système d'information" },
+    { code: 'SY43', name: 'Android Developpement' },
+    { code: 'DE52', name: 'Génie logiciel' },
     {
       code: 'SY41',
       name: 'Informatique embarquée et robotique: Microprocesseurs et microcontrolleurs',
-      image: 'chemistry.jpg',
     },
-    { code: 'WE4B', name: 'Technologies WEB avancées', image: 'chemistry.jpg' },
-    { code: 'VA51', name: "Traitement de l'image", image: 'chemistry.jpg' },
-    { code: 'RS40', name: 'Réseaux et Cybersécurité', image: 'chemistry.jpg' },
-    { code: 'HM40', name: 'Interface Homme Machine', image: 'chemistry.jpg' },
+    { code: 'WE4B', name: 'Technologies WEB avancées' },
+    { code: 'VA51', name: "Traitement de l'image" },
+    { code: 'RS40', name: 'Réseaux et Cybersécurité' },
+    { code: 'HM40', name: 'Interface Homme Machine' },
   ];
 
   constructor() {}
