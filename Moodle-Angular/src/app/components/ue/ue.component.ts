1d7828a97442c4e2f60f202836e0a7f739302008
Akmal <akmalsabri73@gmail.com>
2025-06-05 00:28:04 +0200

    change route for UE cards

--- Moodle-Angular/src/app/components/ue/ue.component.ts
+++ Moodle-Angular/src/app/components/ue/ue.component.ts
@@ -3,31 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ue',
   templateUrl: './ue.component.html',
-  styleUrls: ['./ue.component.css']
+  styleUrls: ['./ue.component.css'],
 })
 export class UeComponent implements OnInit {
-
   courses = [
     {
       title: 'RS40 Reseaux et Cybersecurite',
       color: '#FFD36E',
-      link: '#'
+      link: 'mes-cours',
     },
     {
       title: 'WE4B Technologies WEB avancees',
       color: '#FFD36E',
-      link: '#'
+      link: 'mes-cours',
     },
     {
       title: "SI40 Systeme d'information",
       color: '#FFD36E',
-      link: '#'
-    }
+      link: 'mes-cours',
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
