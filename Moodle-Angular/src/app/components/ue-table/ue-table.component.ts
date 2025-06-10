f288e5cb1a52c63cc68733cc5c52b4123b23dcf4
Akmal <akmalsabri73@gmail.com>
2025-06-10 11:25:45 +0200

    delete ue - page admin

--- Moodle-Angular/src/app/components/ue-table/ue-table.component.ts
+++ Moodle-Angular/src/app/components/ue-table/ue-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ue-table',
@@ -6,20 +6,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./ue-table.component.css'],
 })
 export class UETableComponent {
-  // ues = [
-  //   { code: 'IT44', name: 'Analyse Numérique' },
-  //   { code: 'SI40', name: "Système d'information" },
-  //   { code: 'SY43', name: 'Android Developpement' },
-  //   { code: 'DE52', name: 'Génie logiciel' },
-  //   {
-  //     code: 'SY41',
-  //     name: 'Informatique embarquée et robotique: Microprocesseurs et microcontrolleurs',
-  //   },
-  //   { code: 'WE4B', name: 'Technologies WEB avancées' },
-  //   { code: 'VA51', name: "Traitement de l'image" },
-  //   { code: 'RS40', name: 'Réseaux et Cybersécurité' },
-  //   { code: 'HM40', name: 'Interface Homme Machine' },
-  // ];
   @Input() ues: any[] = [];
+  @Output() deleteUe = new EventEmitter<string>();
+
+  selectedCodeUe: string | null = null;
+
   constructor() {}
+  openDeleteModal(codeUe: string) {
+    this.selectedCodeUe = codeUe;
+  }
 }
