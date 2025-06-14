378145986cf5e637fbbbf7fa4de007e033628d8b
Akmal <akmalsabri73@gmail.com>
2025-06-14 17:19:24 +0200

    update urlUe

--- Moodle-Angular/src/app/backend_urls.ts
+++ Moodle-Angular/src/app/backend_urls.ts
@@ -3,5 +3,5 @@ export const apiUrls = {
   root: 'http://localhost:8800/',
   AuthService: 'http://localhost:8800/backend/auth/',
   user: 'http://localhost:8800/backend/user',
-  ue: 'http://localhost:8800/backend/ues',
+  ue: 'http://localhost:8800/backend/ues/',
 };
