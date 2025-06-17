2e67026255757b3d2267ff0297f186c867d57573
Akmal <akmalsabri73@gmail.com>
2025-06-17 12:59:57 +0200

    abort api - update User (admin)

--- Moodle-Angular/src/app/components/user-table/user-table.component.ts
+++ Moodle-Angular/src/app/components/user-table/user-table.component.ts
@@ -8,11 +8,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class UserTableComponent implements OnInit {
   @Input() users: any[] = [];
   @Output() deleteUser = new EventEmitter<string>();
-  @Output() updateUser = new EventEmitter<any>();
 
   selectedUserId: string | null = null;
-
-  editedUser: any = {};
 
   constructor() {}
 
@@ -20,15 +17,5 @@ export class UserTableComponent implements OnInit {
 
   openDeleteModal(userId: string) {
     this.selectedUserId = userId;
-  }
-
-  openUpdateModal(userId: string) {
-    const user = this.users.find((u) => u._id === userId);
-    this.editedUser = { ...user };
-  }
-
-  emitUpdateUser() {
-    const userToUpdate = { ...this.editedUser };
-    this.updateUser.emit(userToUpdate);
   }
 }
