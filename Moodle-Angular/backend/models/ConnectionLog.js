aec0ab096cf672224e810e1935fb198c7c4e824a
Akmal <akmalsabri73@gmail.com>
2025-06-23 20:09:52 +0200

    create collection ConnectioLog

---
+++ Moodle-Angular/backend/models/ConnectionLog.js
@@ -0,0 +1,11 @@
+// models/ConnectionLog.js
+import mongoose from "mongoose";
+
+const connectionLogSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  email: String,
+  status: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("ConnectionLog", connectionLogSchema);
