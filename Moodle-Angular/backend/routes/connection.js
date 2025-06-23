0c316dd83c85e0a0f8fe82ce3f42ddf1af90c3df
Akmal <akmalsabri73@gmail.com>
2025-06-23 20:10:51 +0200

    route to connectionLog

---
+++ Moodle-Angular/backend/routes/connection.js
@@ -0,0 +1,10 @@
+import { getConnectionLog } from "../controllers/connection-log.js";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", getConnectionLog);
+
+// router.get("/:id", getUserConnectionLog);
+
+export default router;
